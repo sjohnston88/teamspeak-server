8102c48f6db866932d365dab56309c16e0bdafc8
Sam Johnston <samjohnston@notonthehighstreet.com>
2020-01-30 12:02:40 +0000

    wip

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 aws cloudformation deploy \
     --region eu-west-1 \
-    --stack-name teamspeak-server-test \
+    --stack-name teamspeak-server-hi \
     --capabilities CAPABILITY_NAMED_IAM \
     --template-file ./infra.yml \
     --no-fail-on-empty-changeset \
